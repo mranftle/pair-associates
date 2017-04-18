13d9b838f664d7748e6a677ab4aabf0369b97024
mranftle <matthew.ranftle@gmail.com>
2017-04-18 03:12:03 -0500

    reformatting

--- paired-associate-task/src/app/services/auth.service.ts
+++ paired-associate-task/src/app/services/auth.service.ts
@@ -7,7 +7,6 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
-import {UserResponse} from "../entities/userresponse";
 
 @Injectable()
 export class AuthService {
