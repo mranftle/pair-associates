660bf8a1a873fa8f573ec69d2b578672a5029179
mranftle <matthew.ranftle@gmail.com>
2017-05-04 13:36:07 -0500

    settings

--- pairs_api/pairs_api/settings.py
+++ pairs_api/pairs_api/settings.py
@@ -89,7 +89,6 @@ WSGI_APPLICATION = 'pairs_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
