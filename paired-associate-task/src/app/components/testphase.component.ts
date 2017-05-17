9ca11ded724c677dc09a69fa7138b47b8e8c2178
mranftle <matthew.ranftle@gmail.com>
2017-05-17 14:46:39 -0400

    intermediate commit

--- paired-associate-task/src/app/components/testphase.component.ts
+++ paired-associate-task/src/app/components/testphase.component.ts
@@ -40,7 +40,15 @@ export class TestPhaseComponent implements OnInit {
         this.showCorrect=false;
         this.selectedWordPair = this.wordPairs[0];
         this.responseNum = 0;
-        this.timer = setTimeout(() => this.submitPair(), 5000); // TESTING TIME
+
+        //set timer for
+        console.log(this.testPhase);
+        if(this.testPhase == 2 || this.testPhase == 3) {
+          this.timer = setTimeout(() => this.submitPair(), 5000); // TESTING TIME
+        }
+        else if (this.testPhase == 4) {
+          this.timer = setTimeout(() => this.submitPairNoFeedback(), 5000); //TESTING TIME
+        }
         this.responseTime = Date.now();
       }
     );
@@ -79,9 +87,41 @@ export class TestPhaseComponent implements OnInit {
 
   }
 
+  submitPairNoFeedback(): void {
+    var elipsedTime = Date.now() - this.responseTime;
+    var ur = this.response.nativeElement.value || null ;
+    this.response.nativeElement.value = null;
+    var userresponse = {
+      word1: this.selectedWordPair.word1,
+      word2: this.selectedWordPair.word2,
+      response_number:this.responseNum,
+      response: ur,
+      response_time: elipsedTime,
+      test_phase: this.testPhase
+    };
+    this.wordPairService.saveUserResponse(userresponse);
+    this.showCorrect = false;
+    if (this.responseNum < this.wordPairs.length -1 ) {
+        this.responseNum++;
+    } else {
+
+      //testing done, route to next component
+      this.router.navigate(['/instructions']);
+      return;
+    }
+    this.selectedWordPair = this.wordPairs[this.responseNum];
+    this.resetTimerNoFeedback();
+  }
+
   resetTimer() : void {
     clearTimeout(this.timer);
     this.timer = setTimeout(() => this.submitPair(), 5000); // TESTING TIME
+    this.responseTime = Date.now();
+  }
+
+  resetTimerNoFeedback() : void {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => this.submitPairNoFeedback(), 5000); // TESTING TIME
     this.responseTime = Date.now();
   }
 
