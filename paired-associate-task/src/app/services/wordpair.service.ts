8cf34bc70bfa4b82d9cc2ba00a62dacb8cac3f51
mranftle <matthew.ranftle@gmail.com>
2017-06-20 16:33:00 -0500

    https backend requests working

--- paired-associate-task/src/app/services/wordpair.service.ts
+++ paired-associate-task/src/app/services/wordpair.service.ts
@@ -11,9 +11,9 @@ import {Observable} from "rxjs";
 
 @Injectable()
 export class WordPairService {
-  private wordPairUrl = 'http://pairsassociatesapi.servehttp.com/wordpairs';
-  private isTestUrl = 'http://pairsassociatesapi.servehttp.com/istest';
-  private userResponseUrl='http://pairsassociatesapi.servehttp.com/userresponse/';
+  private wordPairUrl = 'https://pairsassociatesapi.servehttp.com/wordpairs';
+  private isTestUrl = 'https://pairsassociatesapi.servehttp.com/istest';
+  private userResponseUrl='https://pairsassociatesapi.servehttp.com/userresponse/';
   constructor(private http: Http) { }
 
   // get word pairs for study and testing
